4f59d8e4c82f505d6b8abff2b15de0329735514d
namantejaswi <tejaswinaman1@gmail.com>
2021-11-03 18:41:47 -0400

    added recurssive binary search

--- binary_search.py
+++ binary_search.py
@@ -1,4 +1,4 @@
-
+import math
 
 def binarysearch(arr,target):
     
@@ -27,3 +27,32 @@ print(binarysearch(l,124))
 
 print(binarysearch(l,44))
 
+# print(math.ceil(2.01))
+#print(math.ceil(2))
+
+def recurssive_bs(arr,x,low,high)->int:
+
+    mid=math.ceil((low+high)/2)
+   
+    if(low>high):
+        return -1
+    
+    elif(arr[mid]==x):
+        print("Found element at index",mid)
+        return mid
+    
+    elif(x>arr[mid]):
+        low=mid+1
+        recurssive_bs(arr,x,low,high)
+        
+    elif(x<arr[mid]):
+        high=mid-1
+        recurssive_bs(arr,x,low,high)
+
+a=[1,2,3,5,6,7]
+        
+recurssive_bs(a,6,0,len(a)-1)
+recurssive_bs(l,124,0,len(l)-1)
+recurssive_bs(l,11,0,len(l)-1)
+
+    
